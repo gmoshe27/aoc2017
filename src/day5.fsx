115ec4bfeafaa9a7d24c7ed98d6baf7cc26726b9
Gad Berger <gad@jet.com>
2017-12-08 09:19:07 -0500

    updated day 5 q1

--- src/day5.fsx
+++ src/day5.fsx
@@ -1,7 +1,7 @@
 open System.IO
 
 module Question1 =
-    let debug = true
+    let debug = false
     let testInput = [| 0; 3; 0; 1; -3 |]
     let input = File.ReadAllLines @"..\input\day5-q1.txt"
 
@@ -12,12 +12,11 @@ module Question1 =
         input.[step] <- curr + 1
 
         if next >= input.Length then
-            if debug then printfn "Final Counts: %A" input
             t
         else
             execute next t input
     
-    input
+    if debug then printfn "final counts: %A" input
 
     let input' = input |> Array.map int
     let answer () = execute 0 0 input'
