26f7befe12af75c377b145a97f0cd8fb5878728f
Gad Berger <gad@jet.com>
2017-12-07 22:26:22 -0500

    day 3 question 2

--- src/day3.fsx
+++ src/day3.fsx
@@ -33,6 +33,70 @@ module Question1 =
         printfn "Total Steps = %d" <| findSteps 1024
         printfn "Total Steps = %d" <| findSteps 277678
 
-Question1.answer ()
+//Question1.answer ()
 
-module Question2 = ()
+module Question2 =
+
+    type Move = Left | Right | Up | Down
+
+    // This is cute, the matrix size is equal to the rounded square of the input number
+    let findMatrixSize input = input |> float |> sqrt |> ceil |> int
+
+    let addSpiral i j (arr:int [,]) =
+        let left = arr.[i, j - 1]
+        let right = arr.[i, j + 1]
+        let up = arr.[i - 1, j]
+        let down = arr.[i + 1, j]
+        let upLeft = arr.[i - 1, j - 1]
+        let downLeft = arr.[i + 1, j - 1]
+        let upRight = arr.[i - 1, j + 1]
+        let downRight = arr.[i + 1, j + 1]
+        left + right + up + down + upLeft + downLeft + upRight + downRight
+
+    let drawSpiral debug input =
+
+        // initialize the matrix with extra padding for our convolution filter
+        let matrixSize = findMatrixSize input
+        let size = matrixSize + 2
+        let midpoint = size / 2
+        let arr = Array2D.init size size (fun _ _-> 0)
+
+        // start with 1 in the center
+        arr.[midpoint, midpoint] <- 1
+
+        // it's not really convolution, but close enough...
+        let rec loop i j steps square move =
+            let ki, kj =
+                match move with
+                | Right -> i, j + 1
+                | Up -> i - 1, j
+                | Left -> i, j - 1
+                | Down -> i + 1, j
+
+            if debug then printfn "addSpiral: %d, %d" ki kj
+
+            arr.[ki, kj] <- arr |> addSpiral ki kj
+            if arr.[ki, kj] > input then
+                arr.[ki, kj]
+            else
+                if debug then printfn "%A" arr
+                let nextMove =
+                    match move with
+                    | Right -> if steps + 1 = square then Move.Up else move
+                    | Up -> if steps + 1 = (square - 2) then Move.Left else move
+                    | Down -> if steps + 1 = (square - 1) then Move.Right else move
+                    | Left -> if steps + 1 = (square - 1) then Move.Down else move
+
+                let square' = if steps + 1 = square then square + 2 else square
+                let steps' = if move <> nextMove then 0 else steps + 1
+
+                if debug then printfn "next move = %A" nextMove
+                loop ki kj steps' square' nextMove
+        
+        let i, j = midpoint, midpoint
+        loop i j 0 1 Move.Right
+
+    let answer debug input = drawSpiral debug input
+
+Question2.answer true 50 |> printfn "Question 2 [%d]: %d" 50
+Question2.answer false 277678 |> printfn "Question 2 [%d]: %d" 27678
