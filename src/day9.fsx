efdd8d8b36311bf09baa3bf9685538182a7c9e0a
Gad Berger <gad@jet.com>
2017-12-11 09:38:07 -0500

    day 9 q1

---
+++ src/day9.fsx
@@ -0,0 +1,48 @@
+open System.IO
+
+type Groups =
+    {
+        Level : int
+        Counts : int list
+        Garbage : bool
+        Ignore : bool
+    }
+with
+    static member zero =
+        {
+            Level = 0
+            Counts = []
+            Garbage = false
+            Ignore = false
+        }
+
+module Question1 =
+    let debug = false
+    let data = if debug then @"..\input\day9-sample.txt" else @"..\input\day9-q1.txt"
+    let input = File.ReadAllLines data
+
+    // This question is similar to finding if a string has balanced parenthesis, which
+    // can be implemented with a stack. The complexity in this question comes with keeping
+    // how deep into the nesting you are.
+
+    let sum (groups:Groups) (c:char) =
+        if groups.Ignore then
+            { groups with Ignore = false }
+        else
+            let garbageOr g = if not groups.Garbage then g else groups
+
+            match c with
+            | '!' -> { groups with Ignore = true }
+            | '{' -> garbageOr { groups with Level = groups.Level + 1 }
+            | '}' -> garbageOr { groups with Level = groups.Level - 1; Counts = [groups.Level] @ groups.Counts }
+            | '<' -> garbageOr { groups with Garbage = true }
+            | '>' -> { groups with Garbage = false }
+            | _ -> groups // ignore, including ',' and garbage
+    
+    // since the sample input has more than one stream, we'll loop through the array of inputs
+    // otherwise, we only need to fold over the stream (no real need for Array.map)
+    let sums = input |> Array.map ( fun stream -> stream |> Seq.fold sum Groups.zero )
+
+    let answer() = sums |> Array.map (fun x -> x.Counts |> List.sum)
+
+Question1.answer () |> printfn "%A"
