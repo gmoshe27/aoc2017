4f0a00d1ca2afc84a043dd58881b1f9be16cc1ca
Gad Berger <gad@jet.com>
2017-12-11 09:46:40 -0500

    day 9 complete

--- src/day9.fsx
+++ src/day9.fsx
@@ -5,6 +5,7 @@ type Groups =
         Level : int
         Counts : int list
         Garbage : bool
+        GarbageCounts : int // for question 2
         Ignore : bool
     }
 with
@@ -13,6 +14,7 @@ with
             Level = 0
             Counts = []
             Garbage = false
+            GarbageCounts = 0
             Ignore = false
         }
 
@@ -46,3 +48,31 @@ module Question1 =
     let answer() = sums |> Array.map (fun x -> x.Counts |> List.sum)
 
 Question1.answer () |> printfn "%A"
+
+module Question2 =
+    let sum (groups:Groups) (c:char) =
+        if groups.Ignore then
+            { groups with Ignore = false }
+        else
+            let garbageOr g =
+                if not groups.Garbage then
+                    g
+                else
+                    { groups with GarbageCounts = groups.GarbageCounts + 1 }
+
+            match c with
+            | '!' -> { groups with Ignore = true }
+            | '{' -> garbageOr { groups with Level = groups.Level + 1 }
+            | '}' -> garbageOr { groups with Level = groups.Level - 1; Counts = [groups.Level] @ groups.Counts }
+            | '<' -> garbageOr { groups with Garbage = true }
+            | '>' -> { groups with Garbage = false }
+            | ',' -> garbageOr groups
+            | _ -> garbageOr groups // ignore, including ',' and garbage
+    
+    // since the sample input has more than one stream, we'll loop through the array of inputs
+    // otherwise, we only need to fold over the stream (no real need for Array.map)
+    let sums = Question1.input |> Array.map ( fun stream -> stream |> Seq.fold sum Groups.zero )
+
+    let answer () = sums |> Array.map (fun x -> x.GarbageCounts)
+
+Question2.answer () |> printfn "%A"
